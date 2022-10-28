f788a5506a4411efb7cf63547ba476118a114915
toshio-nakayama <toshio.h1990@gmail.com>
2022-10-28 17:09:49 +0900

    2022/10/28 17:09

---
+++ src/main/java/com/example/dynamicsql/database/User.java
@@ -0,0 +1,66 @@
+package com.example.dynamicsql.database;
+
+import javax.annotation.Generated;
+import javax.validation.constraints.*;
+
+public class User {
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", comments="Source field: user.id")
+    private Integer id;
+
+    @NotBlank
+    @Size(max = 60)
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", comments="Source field: user.name")
+    private String name;
+
+    @NotNull
+    @Min(18)
+    @Max(150)
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", comments="Source field: user.age")
+    private Integer age;
+
+    @NotBlank
+    @Size(max = 254)
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", comments="Source field: user.profile")
+    private String profile;
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", comments="Source field: user.id")
+    public Integer getId() {
+        return id;
+    }
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", comments="Source field: user.id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", comments="Source field: user.name")
+    public String getName() {
+        return name;
+    }
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", comments="Source field: user.name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", comments="Source field: user.age")
+    public Integer getAge() {
+        return age;
+    }
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", comments="Source field: user.age")
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", comments="Source field: user.profile")
+    public String getProfile() {
+        return profile;
+    }
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", comments="Source field: user.profile")
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+}
